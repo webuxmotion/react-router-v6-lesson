0d73cb9b7d5e610fcc046907693fbb93bc676871
Andrii Pereverziev <a.pereverziev@mail.enamine.net>
2021-07-12 16:18:19 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { css } from '@emotion/css';
+
+import Products from './Products/Products';
+import Admin from './Admin/Admin';
+
+const AppStyles = css`
+  margin: 50px auto;
+  width: 380px;
+  .Container {
+    background: #1d1e26;
+    border: 4px solid #9580ff;
+    border-radius: 6px;
+    padding: 25px;
+  }
+`;
+
+const App = () => {
+  return <div className={AppStyles}>
+    <Router>
+      <div className="Container">
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<div>Not Found</div>} />
+        </Routes>
+      </div>
+    </Router>
+  </div>;
+};
+
+export default App;
