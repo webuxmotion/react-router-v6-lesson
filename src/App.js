ccdb1cd5049be2418e116e7abb6fcc7931f79a25
Andrii Pereverziev <a.pereverziev@mail.enamine.net>
2021-07-12 16:28:15 +0300

    Create Nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { css } from '@emotion/css';
 
+import Nav from './Common/Nav';
 import Products from './Products/Products';
 import Admin from './Admin/Admin';
 
@@ -20,10 +21,12 @@ const App = () => {
   return <div className={AppStyles}>
     <Router>
       <div className="Container">
+        <Nav />
         <Routes>
           <Route path="/" element={<Products />} />
           <Route path="/admin" element={<Admin />} />
-          <Route path="*" element={<div>Not Found</div>} />
+          {/* <Route path="*" element={<div>Not Found</div>} /> */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
     </Router>
---
+++ src/Common/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Nav = () => (
+  <nav>
+    Nav
+  </nav>
+);
+
+export default Nav;
